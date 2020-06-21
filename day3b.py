84f9a079099d0dc78bbd6558401b5fdb3905befd
SriAthishya <66429365+SriAthishya@users.noreply.github.com>
2020-06-21 21:34:13 +0530

    Update day3b.py

--- day3b.py
+++ day3b.py
@@ -52,7 +52,13 @@ def vowels(str1):
   a=sorted(dic.values(),reverse=True)
   return a
 
-def spring_mat(m1):
+def spring_mat(m1,m2):
+  m3=[[0,0],
+      [0,0]]
+  for i in range(len(m1)):
+      for j in range(len(m2)):
+        for k in range(len(m3)):
+          m3[i][j]+=m1[i][k]*m2[k][j]
   str1=""
   a=len(m1)//2
   for i in range(0,a):
